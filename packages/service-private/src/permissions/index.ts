1ffb3d38eb020f34a25a469e2d9eb79663a04218
Jozef Hruška <jo.hruska27@gmail.com>
2020-05-27 19:52:35 +0200

    fix(frontend): dashboard user error

--- packages/service-private/src/permissions/index.ts
+++ packages/service-private/src/permissions/index.ts
@@ -3,32 +3,37 @@ import { shield, not, and } from 'graphql-shield';
 import { Context } from '..';
 import { isAuthenticated, canManageCollection } from './rules';
 
-export const permissions = shield<unknown, Context>({
-  Query: {
-    /* Users */
-    users: isAuthenticated,
+export const permissions = shield<unknown, Context>(
+  {
+    Query: {
+      /* Users */
+      users: isAuthenticated,
 
-    /* Collections */
-    collections: isAuthenticated,
-    collection: isAuthenticated,
+      /* Collections */
+      collections: isAuthenticated,
+      collection: isAuthenticated,
 
-    /* Components */
-    components: isAuthenticated,
-    component: isAuthenticated,
+      /* Components */
+      components: isAuthenticated,
+      component: isAuthenticated,
+    },
+    Mutation: {
+      /* Users */
+      updateOneUser: isAuthenticated,
+      signIn: not(isAuthenticated, 'Request is only available to non-authenticated users.'),
+
+      /* Collections */
+      createOneCollection: isAuthenticated,
+      updateOneCollection: and(isAuthenticated, canManageCollection),
+      deleteOneCollection: and(isAuthenticated, canManageCollection),
+
+      /* Components */
+      createOneComponent: isAuthenticated,
+      updateOneComponent: isAuthenticated,
+      deleteOneComponent: isAuthenticated,
+    },
   },
-  Mutation: {
-    /* Users */
-    updateOneUser: isAuthenticated,
-    signIn: not(isAuthenticated, 'Request is only available to non-authenticated users.'),
-
-    /* Collections */
-    createOneCollection: isAuthenticated,
-    updateOneCollection: and(isAuthenticated, canManageCollection),
-    deleteOneCollection: and(isAuthenticated, canManageCollection),
-
-    /* Components */
-    createOneComponent: isAuthenticated,
-    updateOneComponent: isAuthenticated,
-    deleteOneComponent: isAuthenticated,
-  },
-});
+  {
+    debug: true,
+  }
+);
